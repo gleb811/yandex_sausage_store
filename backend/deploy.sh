1c04a286f79fbbd80bfbffeedf468fb406fc159f
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2023-12-12 17:26:52 +0000

    Update backend/deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,3 +3,4 @@
 set -xe
 
 sudo -u backend curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /opt/sausage-store/bin/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo -u backend ln -vfns /opt/sausage-store/bin/sausage-store-${VERSION}.jar /opt/sausage-store/bin/sausage-store-backend.jar
