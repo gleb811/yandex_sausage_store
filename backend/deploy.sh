ee5fca76f692248c8646401c12d7b68fe50c249d
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2023-12-12 05:53:06 +0000

    Update backend/.gitlab-ci.yml, backend/deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,4 +4,4 @@
 #set -xe
 #скачиваем артефакт
 echo "${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar"
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo -u backend curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /opt/sausage-store/bin/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
