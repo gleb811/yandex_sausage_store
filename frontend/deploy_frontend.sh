6b59abe8a206ab7f66c0eca45a9f912755654034
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2023-12-14 05:28:19 +0000

    Update frontend/.gitlab-ci.yml, frontend/deploy_frontend.sh

--- frontend/deploy_frontend.sh
+++ frontend/deploy_frontend.sh
@@ -4,3 +4,12 @@ set -xe
 
 
 sudo -u frontend curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /opt/sausage-store/static/dist/sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL}/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
+
+cd /opt/sausage-store/static/dist
+tar -xvf sausage-store-${VERSION}.tar.gz
+rm -f sausage-store-${VERSION}.tar.gz
+
+sudo mv /home/${DEV_USER}/sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+
+sudo systemctl daemon-reload
+sudo systemctl restart sausage-store-backend
