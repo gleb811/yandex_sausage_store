2bcf7f7447fe8a5c7e09147029293646bbf14bd7
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2023-12-14 05:34:32 +0000

    Update frontend/deploy_frontend.sh

--- frontend/deploy_frontend.sh
+++ frontend/deploy_frontend.sh
@@ -6,8 +6,8 @@ set -xe
 sudo -u frontend curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /opt/sausage-store/static/dist/sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL}/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
 
 cd /opt/sausage-store/static/dist
-tar -xvf sausage-store-${VERSION}.tar.gz
-rm -f sausage-store-${VERSION}.tar.gz
+sudo - u frontend tar -xvf sausage-store-${VERSION}.tar.gz
+sudo - u frontend rm -f sausage-store-${VERSION}.tar.gz
 
 sudo mv /home/${DEV_USER}/sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 
