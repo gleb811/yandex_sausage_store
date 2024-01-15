f6438528f5ae4e5f8957fbf9f0205cbae321ab13
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-01-15 01:44:05 +0000

    Add new file

---
+++ backend/src/main/resources/db/migration/V003__create_index.sql
@@ -0,0 +1,2 @@
+INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 10000000) s(i);
+INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 10000000) s(i);
