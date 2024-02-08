9c0ecf0b44706a0561ae5115693318ecd92f49a7
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-08 16:50:02 +0000

    Update file deploy-backend-docker.sh

--- backend/deploy-backend-docker.sh
+++ backend/deploy-backend-docker.sh
@@ -4,7 +4,7 @@ export SPRING_CLOUD_VAULT_TOKEN=${SPRING_CLOUD_VAULT_TOKEN}
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
-sudo docker run --rm -d --name sausage-backend  --pull always\
+sudo docker run --name sausage-backend  --pull always\
      --env-file .env \
      --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-backend:latest
