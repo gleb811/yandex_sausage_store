0d8c5f3f6a13a33bb32e6e5168f54363e913de65
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-13 09:17:57 +0000

    Update file deploy-backend-docker.sh

--- backend/deploy-backend-docker.sh
+++ backend/deploy-backend-docker.sh
@@ -6,7 +6,6 @@ sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
 sudo docker run -d --name sausage-backend  --pull always\
      --env SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME} \
-     --env SPRING_CLOUD_VAULT_TOKEN=${SPRING_CLOUD_VAULT_TOKEN} \
      --env SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD} \
      --env SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI} \
      --env SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL} \
