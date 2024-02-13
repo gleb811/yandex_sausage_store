df78a524e66fd4eefb54596030b4c982aeba8a0a
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-13 06:55:38 +0000

    Update file deploy-backend-docker.sh

--- backend/deploy-backend-docker.sh
+++ backend/deploy-backend-docker.sh
@@ -9,5 +9,5 @@ sudo docker run -d --name sausage-backend  --pull always\
      "${CI_REGISTRY_IMAGE}"/sausage-backend
 sudo docker image prune -f
 echo ${SPRING_CLOUD_VAULT_TOKEN}
-
+echo ${SPRING_DATASOURCE_PASSWORD}
 echo ${VAULT_TOKEN}
