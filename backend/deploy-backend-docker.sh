b001a8af0915acb8ff19dcea3be4be8b2859d9f7
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-13 09:08:29 +0000

    Update 2 files
    - /backend/.gitlab-ci.yml
    - /backend/deploy-backend-docker.sh

--- backend/deploy-backend-docker.sh
+++ backend/deploy-backend-docker.sh
@@ -5,7 +5,11 @@ sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
 sudo docker run -d --name sausage-backend  --pull always\
-     --env-file <( env| cut -f1 -d= ) \
+     --env SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME} \
+     --env SPRING_CLOUD_VAULT_TOKEN=${SPRING_CLOUD_VAULT_TOKEN} \
+     --env SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD} \
+     --env SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI=} \
+     --env SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
      --network=sausage_network \
      "${CI_REGISTRY_IMAGE}"/sausage-backend
 sudo docker image prune -f
