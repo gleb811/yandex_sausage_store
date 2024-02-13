3a60a86f96a5065b8b13aef54207b8dbdaed07fd
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-13 06:25:44 +0000

    Update 3 files
    - /backend/src/main/resources/application.properties
    - /backend/.gitlab-ci.yml
    - /backend/deploy-backend-docker.sh

--- backend/deploy-backend-docker.sh
+++ backend/deploy-backend-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 set -xe
-export SPRING_CLOUD_VAULT_TOKEN=hvs.K6ABHGzeP65sn1HGVMvbFajY
+SPRING_CLOUD_VAULT_TOKEN=${SPRING_CLOUD_VAULT_TOKEN}
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
