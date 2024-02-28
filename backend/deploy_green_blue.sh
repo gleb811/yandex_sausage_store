3be07141d6a8fd83490837294c167410acfa637c
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-28 19:39:17 +0000

    Update 2 files
    - /backend/deploy_green_blue.sh
    - /docker-compose.yml

--- backend/deploy_green_blue.sh
+++ backend/deploy_green_blue.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-sudo docker network create -d bridge sausage_network || true
+#sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f $(docker ps -qa --filter "name=sausage-store-backend") || true
 sudo docker compose --env-file deploy.env up backend -d --pull "always" --force-recreate
 #sudo docker compose --env-file deploy.env up --scale backend=2 -d --no-recreate
