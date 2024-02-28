4bbb06b176851395278cba89c5c77cef3ffc5580
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-28 16:07:57 +0000

    Update 2 files
    - /backend/deploy_green_blue.sh
    - /backend/deploy-backend-docker.sh

--- backend/deploy_green_blue.sh
+++ backend/deploy_green_blue.sh
@@ -2,3 +2,6 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker rm -f $(docker ps -qa --filter "name=sausage-store-backend") || true
+sudo docker compose --env-file deploy.env up backend -d --pull "always" --force-recreate
+sudo docker compose --env-file deploy.env up --scale backend=2 -d --no-recreate
+sudo docker image prune -f
