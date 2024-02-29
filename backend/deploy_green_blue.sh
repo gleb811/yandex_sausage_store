d00f30221582d00c9f9f5ac3964b71c3087bc4c8
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-29 07:06:19 +0000

    Update file deploy_green_blue.sh

--- backend/deploy_green_blue.sh
+++ backend/deploy_green_blue.sh
@@ -3,7 +3,7 @@ set -xe
 docker --context remote compose --env-file deploy.env up backend-green -d --pull "always" --force-recreate
 #docker --context remote compose --env-file deploy.env up --scale backend-green=2 -d --no-recreate
 docker --context remote compose --env-file deploy.env up backend-blue -d --pull "always" --force-recreate
-#docker --context remote compose --env-file deploy.env up --scale backend-blue=2 -d --no-recreate
+docker --context remote compose --env-file deploy.env up --scale backend-green=2 -d --no-recreate
 #sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 #sudo docker network create -d bridge sausage_network || true
 #sudo docker rm -f $(docker ps -qa --filter "name=sausage-store-backend") || true
