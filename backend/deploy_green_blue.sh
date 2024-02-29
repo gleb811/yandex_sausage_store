4f8f1e49f84e466b70e7f9e5aab481553c35c26f
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-29 18:46:11 +0000

    Update file deploy_green_blue.sh

--- backend/deploy_green_blue.sh
+++ backend/deploy_green_blue.sh
@@ -5,7 +5,7 @@ docker --context remote compose --env-file deploy.env up backend-green -d --pull
 #docker --context remote compose --env-file deploy.env up backend-blue -d --pull "always" --force-recreate
 docker --context remote compose --env-file deploy.env up --scale backend-green=2 backend-green -d --no-recreate
 #docker --context remote compose --env-file deploy.env up --scale backend-blue=2 backend-blue -d --no-recreate
-if [docker ps -qa --filter "name=sausage-store-backend-blue"]
+if [$(docker ps -qa --filter "name=sausage-store-backend-blue") -ne ""]
 then 
   docker rm -f $(docker ps -qa --filter "name=sausage-store-backend-blue") || true
 else
