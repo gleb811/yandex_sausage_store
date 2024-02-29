bca8ab8caeaaed48dcd0b6373d5660441e368b19
std-024-07 std-024-07 <std-024-07@praktikum-services.ru>
2024-02-29 18:14:03 +0000

    Update 2 files
    - /backend/.gitlab-ci.yml
    - /backend/deploy_green_blue.sh

--- backend/deploy_green_blue.sh
+++ backend/deploy_green_blue.sh
@@ -2,12 +2,13 @@
 set -xe
 docker --context remote compose --env-file deploy.env up backend-green -d --pull "always" --force-recreate
 #docker --context remote compose --env-file deploy.env up --scale backend-green=2 -d --no-recreate
-docker --context remote compose --env-file deploy.env up backend-blue -d --pull "always" --force-recreate
+#docker --context remote compose --env-file deploy.env up backend-blue -d --pull "always" --force-recreate
 docker --context remote compose --env-file deploy.env up --scale backend-green=2 backend-green -d --no-recreate
-docker --context remote compose --env-file deploy.env up --scale backend-blue=2 backend-blue -d --no-recreate
-#sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-#sudo docker network create -d bridge sausage_network || true
-#sudo docker rm -f $(docker ps -qa --filter "name=sausage-store-backend") || true
-#sudo docker compose --env-file deploy.env up backend -d --pull "always" --force-recreate
-#sudo docker compose --env-file deploy.env up --scale backend=2 -d --no-recreate
-#sudo docker image prune -f
+#docker --context remote compose --env-file deploy.env up --scale backend-blue=2 backend-blue -d --no-recreate
+if docker ps -qa --filter "name=sausage-store-backend-blue";
+then 
+  docker rm -f $(docker ps -qa --filter "name=sausage-store-backend-blue") || true
+else
+  docker --context remote compose --env-file deploy.env up backend-blue -d --pull "always" --force-recreate
+  docker --context remote compose --env-file deploy.env up --scale backend-blue=2 backend-blue -d --no-recreate
+fi
